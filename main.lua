91d824909c6e3d2c1f0484e9e07c8cee9775ade2
unknown <russmbiz@gmail.com>
2020-10-10 23:43:46 -0500

    sdf

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
 function love.draw()
-    love.graphics.print("Hello World!", 400, 300)
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.circle("fill", 300, 300, 50, 100) -- Draw white circle with 100 segments.
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", 300, 300, 50, 5)   -- Draw red circle with five segments.
 end
